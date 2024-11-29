5e0a710e915ca4ecf2c870688aae31e471746558
Papryczka12 <lukasz.bok@infotech.edu.pl>
2024-11-29 12:28:24 +0100

    Create 1MainActivity.kt

---
+++ 1MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.emepaef
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val webView: WebView = findViewById(R.id.webView)
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://www.infotech.edu.pl")
+    }
+}
